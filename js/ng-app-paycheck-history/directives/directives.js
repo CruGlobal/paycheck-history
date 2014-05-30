2042cdbf914475ddf7d43ce1940b4024c3776320
Matt Drees <matt.drees@cru.org>
2014-05-30 15:57:21 -0600

    remove unused mpga directive

--- js/ng-app-paycheck-history/directives/directives.js
+++ js/ng-app-paycheck-history/directives/directives.js
@@ -13,41 +13,5 @@
         replace: true,
         templateUrl: 'js/ng-app-paycheck-history/templates/year-summary.html'
       }
-    }).
-    directive('nvPiechart',function () {
-      return {
-        scope:{
-          input:'='
-        },
-        link:function (scope, element, attrs) {
-          scope.$watch('input', function(newInput, oldInput) {
-            if(!newInput) {
-              return;
-            }
-
-            nv.addGraph(function() {
-              var chart = nv.models.pieChart()
-                .x(function(d) { return d.label })
-                .y(function(d) { return d.value })
-                .tooltips(true)
-                .tooltipContent(function(key, y, e, graph) {
-                  return '<div class="pie-tooltip">' +
-                    '<span class="pie-value">' + y + ' Partners' + '</span> ' +
-                    '</div>';
-                })
-                .showLabels(false);
-
-              chart.valueFormat(d3.format(',.0f'));
-
-              d3.select(element[0])
-                .datum(scope.input)
-                .transition().duration(1200)
-                .call(chart);
-
-              return chart;
-            });
-          });
-        }
-      }
     });
 })();
