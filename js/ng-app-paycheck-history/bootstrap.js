621db58a7e13cf1324a0c458a93cfcca91d72eeb
Bill Randall <william.randall@cru.org>
2015-07-10 16:17:05 -0400

    Working version of pull_from_webserver

---
+++ js/ng-app-paycheck-history/bootstrap.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.bootstrap(document, ['paycheckHistory', 'ngResource']);
--- js/ng-app-paycheck-history/environment.js
+++ js/ng-app-paycheck-history/environment.js
@@ -4,12 +4,12 @@
  * Indicates whether the apps should use test data instead of getting data from WSAPI.
  * This is used by several of the services that load data.
  */
-var mockServiceData = false;
+var mockServiceData = true;
 
 (function () {
   angular.module('paycheckHistory')
-//    .constant('pshrUriBase', 'https://pshrstage.cru.org/psp/hcm92s')
-    .constant('pshrUriBase', 'https://pshr.cru.org/psp/hcm92p')
+    .constant('pshrUriBase', 'https://pshrstage.cru.org/psp/hcm92s')
+    //.constant('pshrUriBase', 'https://pshr.cru.org/psp/hcm92p')
     .constant('wsapiUriBase', 'https://wsapi.cru.org')
   ;
 
